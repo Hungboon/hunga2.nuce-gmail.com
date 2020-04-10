6bf4f9b3a7fb2930fad6c7cb947e39d4b100ce77
Hungboon <hunga2.nuce@gmail.com>
2020-04-10 19:54:22 +0900

    add intamgiac.rb file

---
+++ intamgiac.rb
@@ -0,0 +1,14 @@
+n = 10
+for i in 1..n
+    for j in  1..n-i
+        print(" " * 2)
+    end
+
+    for a in 1..(2*i-1)
+            print("*")
+            print(" ")
+    end
+    print("\n")
+end
+
+
